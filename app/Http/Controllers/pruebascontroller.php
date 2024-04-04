b9f08bde764361cef556ed02fa96ffc9f172974f
Javier Adolfo Sanchez Espinoza <javier.sanchez1815@uppuebla.edu.mx>
2024-04-03 23:07:00 -0600

    Se agrego la vista 1

---
+++ app/Http/Controllers/pruebascontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pruebascontroller extends Controller
+{
+    public function vista1()
+    {
+        return view('vista1');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\pruebascontroller;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('vista1',[pruebascontroller::class,'vista1'])->name('vista1');
+
+
+Route::get('altamascotas',[mascotascontroller::class,'altamascotas'])->name('altamascotas');
